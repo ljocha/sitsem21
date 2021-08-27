a33691fb600542cc739f5f25c8c66f2ae6dfa384
Ales Krenek <ljocha@ics.muni.cz>
2021-08-27 19:07:31 +0200

    intel vs std

--- run.sh
+++ run.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-cnt=$(ls -t .local/share/jupyter/runtime/nbserver-*-open.html | wc -l)
-export cnt
+docker run -ti -e OMP_NUM_THREADS=1 -e HOME=/work -u $(id -u) -p 8888:8888 -v $PWD:/work ljocha/sitsem21:$1
 
-(
-cnt2=$cnt
-while [ $cnt = $cnt2 ]; do
-	sleep .2
-	cnt2=$cnt
-	cnt=$(ls -t .local/share/jupyter/runtime/nbserver-*-open.html | wc -l)
-done
-
-f=$(ls -t .local/share/jupyter/runtime/nbserver-*-open.html | head -1)
-open file://$PWD/$f
-) &
-
-docker run -ti -e HOME=/work -u $(id -u) -p 127.0.0.1:8888:8888 -v $PWD:/work ljocha/sitsem21
-
-wait
