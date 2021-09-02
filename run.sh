1bb23d841eda39f9ae33b2676f75894584364d39
Ales Krenek <ljocha@ics.muni.cz>
2021-09-02 11:03:02 +0200

    isomap

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker run -ti -e OMP_NUM_THREADS=1 -e HOME=/work -u $(id -u) -p 8888:8888 -v $PWD:/work ljocha/sitsem21:$1
+flavor=${1:-std}
+port=${2:-8888}
+
+docker run -ti -e OMP_NUM_THREADS=1 -e HOME=/work -u $(id -u) -p $port:$port -v $PWD:/work ljocha/sitsem21:$flavor
 
