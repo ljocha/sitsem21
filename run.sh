6cfe300bc1473a14189e0cdd05a2697bb62b0c35
Ales Krenek <ljocha@ics.muni.cz>
2021-09-07 11:36:31 +0200

    porty

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 flavor=${1:-std}
 port=${2:-8888}
 
-docker run -ti -e OMP_NUM_THREADS=1 -e HOME=/work -u $(id -u) -p $port:$port -v $PWD:/work ljocha/sitsem21:$flavor
+docker run -ti -e OMP_NUM_THREADS=1 -e HOME=/work -e port=$port -u $(id -u) -p $port:$port -v $PWD:/work ljocha/sitsem21:$flavor
 
